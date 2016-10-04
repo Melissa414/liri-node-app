2233daaa9c849aaaeead1e77ff6946465cd29326
Melissa414 <Missy41491@gmail.com>
2016-10-03 23:23:27 -0400

    all basic files added

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '3yRnUOxfu9R1yylWEAzNnw7Md',
+  consumer_secret: 'wEPaaN0g3GgjYFIP3UcKbjwoMlYGK8AggJI5HxzAKBXGdrNLKn',
+  access_token_key: '758132074654081024-Nm0afm9FtmfMZqKgQeado6635ER4RmQ',
+  access_token_secret: 'bKnuP39gQ7Ad3Ugn4TsZgVr62s0TgEbmQH13QCAN1OXsW',
+}
